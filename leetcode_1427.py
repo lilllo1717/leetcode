70f5133900d9d107e7a3eebf5baf8796ac3c99b4
lilllo1717 <tignatova001@gmail.com>
2026-01-08 15:40:29 +0100

    fixed sol

--- leetcode_1427.py
+++ leetcode_1427.py
@@ -8,6 +8,21 @@ class Solution:
                 s = s[n - pair[1]%n :] + s[:n - pair[1]%n] 
         return(s)
 
+
+class Solution2:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        n = len(s)
+        net = 0
+
+        for direction, amount in shift:
+            if direction == 0:
+                net -= amount
+            else:
+                net += amount
+        net %= n
+
+        return s[n-net:] + s[:n-net]
+
 if __name__ == "__main__":
     sol = Solution()
     print(sol.stringShift("abcdefg", [[1,1],[1,1],[0,2],[1,3]]))
